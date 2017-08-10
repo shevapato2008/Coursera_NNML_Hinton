b5343ca903a5bd467210b0058c9727110ce8b053
shevapato2008 <shevapato2008@gmail.com>
2017-08-10 14:50:39 -0400

    Updated.

--- Assignments/Assignment2/train.m
+++ Assignments/Assignment2/train.m
@@ -78,7 +78,7 @@ for epoch = 1:epochs
     % MEASURE LOSS FUNCTION.
     CE = -sum(sum(...
       expanded_target_batch .* log(output_layer_state + tiny))) / batchsize;
-    count =  count + 1;
+    count = count + 1;
     this_chunk_CE = this_chunk_CE + (CE - this_chunk_CE) / count;
     trainset_CE = trainset_CE + (CE - trainset_CE) / m;
     fprintf(1, '\rBatch %d Train CE %.3f', m, this_chunk_CE);
@@ -100,28 +100,35 @@ for epoch = 1:epochs
 
     %% HIDDEN LAYER.
     % FILL IN CODE. Replace the line below by one of the options.
-    embed_to_hid_weights_gradient = zeros(numhid1 * numwords, numhid2);
+    % embed_to_hid_weights_gradient = zeros(numhid1 * numwords, numhid2);
     % Options:
     % (a) embed_to_hid_weights_gradient = back_propagated_deriv_1' * embedding_layer_state;
     % (b) embed_to_hid_weights_gradient = embedding_layer_state * back_propagated_deriv_1';
     % (c) embed_to_hid_weights_gradient = back_propagated_deriv_1;
     % (d) embed_to_hid_weights_gradient = embedding_layer_state;
+    % Choose (b)
+    embed_to_hid_weights_gradient = embedding_layer_state * back_propagated_deriv_1';
+    
 
     % FILL IN CODE. Replace the line below by one of the options.
-    hid_bias_gradient = zeros(numhid2, 1);
+    % hid_bias_gradient = zeros(numhid2, 1);
     % Options
     % (a) hid_bias_gradient = sum(back_propagated_deriv_1, 2);
     % (b) hid_bias_gradient = sum(back_propagated_deriv_1, 1);
     % (c) hid_bias_gradient = back_propagated_deriv_1;
     % (d) hid_bias_gradient = back_propagated_deriv_1';
+    % Choose (a)
+    hid_bias_gradient = sum(back_propagated_deriv_1, 2);
 
     % FILL IN CODE. Replace the line below by one of the options.
-    back_propagated_deriv_2 = zeros(numhid2, batchsize);
+    % back_propagated_deriv_2 = zeros(numhid2, batchsize);
     % Options
     % (a) back_propagated_deriv_2 = embed_to_hid_weights * back_propagated_deriv_1;
     % (b) back_propagated_deriv_2 = back_propagated_deriv_1 * embed_to_hid_weights;
     % (c) back_propagated_deriv_2 = back_propagated_deriv_1' * embed_to_hid_weights;
     % (d) back_propagated_deriv_2 = back_propagated_deriv_1 * embed_to_hid_weights';
+    % Choose (a)
+    back_propagated_deriv_2 = embed_to_hid_weights * back_propagated_deriv_1;
 
     word_embedding_weights_gradient(:) = 0;
     %% EMBEDDING LAYER.
